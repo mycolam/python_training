690206a99b5465da5d4317808372cdf846a56d0a
mycolam <mycolam@yandex.ru>
2017-08-20 15:31:31 +0300

    Создана "интелектуальная" фикстура

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,13 +3,9 @@ from model.contact import Contact
 
 
 def test_modify_contact_first_name(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="Cola"))
-    app.session.logout()
 
 
 def test_modify_contact_last_name(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(lastname="Coca"))
-    app.session.logout()
 
