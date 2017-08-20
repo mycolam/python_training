5cec795d60a5fa9f385809e45d8abcac1e523d5e
mycolam <mycolam@yandex.ru>
2017-08-20 17:56:35 +0300

    Доработаны тесты для удаления и модификации групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -80,3 +80,8 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
 
+    def count(self):
+        wd = self.app.wd
+        self.open_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,9 +3,13 @@ from model.contact import Contact
 
 
 def test_modify_contact_first_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="Cola"))
 
 
 def test_modify_contact_last_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(lastname="test"))
     app.contact.modify_first_contact(Contact(lastname="Coca"))
 
