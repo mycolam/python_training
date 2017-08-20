7578c032cb156c42594a24ee73c51c98da313847
mycolam <mycolam@yandex.ru>
2017-08-20 18:20:48 +0300

    Оптимизированы операции, в которых осуществляются переходы между страницами

--- fixture/contact.py
+++ fixture/contact.py
@@ -4,15 +4,18 @@ class ContactHelper:
 
     def open_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("photo")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def return_to_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home page").click()
+        if not (wd.current_url.endswith("") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home page").click()
 
     def create(self, contact):
         wd = self.app.wd
