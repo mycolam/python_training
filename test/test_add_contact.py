05b6a4f2e1c1f97585d43beaa6741726b5171064
mycolam <mycolam@yandex.ru>
2017-09-13 23:05:17 +0300

    Реализовал тест проверки всех полей контакта на главной странице

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]).strip()
+
+
+testdata = [
+    Contact(firstname=random_string("firstname", 10), middlename="bbbb", lastname=random_string("lastname", 10),
+            nickname="dddd", title="eeee", company="ffff", address=random_string("address", 20), homephone="1111111",
+            mobilephone="2222222", workphone="3333333", fax="4444444", email1="email1@abc.com", email2="email2@abc.com",
+            email3="email3@abc.com", homepage="abrvalh.abc.com", birthday="1", birthmonth="January", birthyear="1999",
+            annivday="2", annivmonth="February", annivyear="2000", sec_address="sec address", sec_home="home",
+            sec_notes="notes")
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact("aaaa", "bbbb", "cccc", "dddd", "eeee", "ffff", "gggg", "1111111", "2222222", "3333333",
-                      "4444444", "email1@abc.com", "email2@abc.com", "email3@abc.com", "abrvalh.abc.com", "1",
-                      "January", "1999", "2", "February", "2000", "sec address", "home", "notes")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
